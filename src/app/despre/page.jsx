493cf0e44fd99f3341c9fb0c9c189af79ec4d166
Robert Stefan <lita.robert.stefan@gmail.com>
2025-03-29 15:52:04 +0200

    add despre

--- src/app/despre/page.jsx
+++ src/app/despre/page.jsx
@@ -42,7 +42,7 @@ export const metadata = {
     "DRT Electric - Furnizor de servicii electrice profesionale în România",
 }
 
-export default function About() {
+export default function Despre() {
   return (
     <Container className="mt-16 sm:mt-32">
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
@@ -101,4 +101,4 @@ export default function About() {
       </div>
     </Container>
   )
-}
+} 
